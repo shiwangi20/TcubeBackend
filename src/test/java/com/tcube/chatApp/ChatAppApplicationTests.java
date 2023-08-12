661cecc998e80f869a98eaaa9c819c4e37afb106
shiwangi20 <gargshiwangi2703@gmail.com>
2023-08-12 14:43:59 +0530

    Initial Setup

---
+++ src/test/java/com/tcube/chatApp/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcube.chatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
